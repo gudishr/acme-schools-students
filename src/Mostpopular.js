7fa7600441361ac01c3568fb295cd8b10af3eb81
shruti gudi <shrutigudi@gmail.com>
2019-10-09 18:31:06 -0700

    most popular

---
+++ src/Mostpopular.js
@@ -0,0 +1,19 @@
+// import { React } from 'react'
+// import { connect } from 'react-redux'
+
+// const _Mostpopular = ({ schools }) => {
+//     const result = schools.reduce((acc, v) => {
+//       school.students.length > acc.students.length ? acc = school : acc
+//     }, {})
+//     console.log(result)
+// }
+
+// const mapStateToProps = ({ schools }) => {
+//   return (
+//     { schools }
+//   )
+// }
+
+// const Mostpopular = connect(mapStateToProps)(_Mostpopular)
+
+// export default Mostpopular
--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 const _Nav = ({ students, schools }) => {
+
+  const _schools = schools.map(school => {
+    const _students = students.filter(student => student.schoolId === school.id)
+    return {...school, num : _students }
+  })
+
+  let mostpopular = _schools[0]
+
+  _schools.forEach(school => {
+    school.num > mostpopular.num ? mostpopular = school : mostpopular
+  })
+
+  console.log(mostpopular)
+
   return(
     <nav>
       <NavLink to="/students">Students ({students.length})</NavLink>
       <NavLink to="/schools">Schools ({schools.length})</NavLink>
+      <NavLink to={ mostpopular ? `/schools/${mostpopular.id}` : "#" }>Most Popular</NavLink>
     </nav>
 )}
 
@@ -14,7 +29,7 @@ const Nav = connect(({ students, schools })=> {
   return {
     students,
     schools
-  }
+  } 
 })(_Nav)
 
 export default Nav
